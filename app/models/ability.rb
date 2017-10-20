a340da2cbc255ccb6560245d33e61c4cef027782
nurlanemir <nurlan.emiroff@gmail.com>
2017-10-20 14:30:12 +0200

    Add association between restaurant and dish

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,6 +11,7 @@ class Ability
       can :create, Restaurant
       can :manage, Menu, restaurant_id: admin.restaurants.ids
       can :create, Menu
+      can :manage, Dish, restaurant_id: admin.restaurants.ids
+    end
     end
   end
-end
