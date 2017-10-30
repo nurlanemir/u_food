7737a8e1fa85a73f95771f3644114c5ab4f6f947
basil <basil.mawejje@gmail.com>
2017-10-30 18:28:10 +0200

    Made restaurant_api spec pass

--- spec/request/api/v1/restaurant_spec.rb
+++ spec/request/api/v1/restaurant_spec.rb
@@ -1,5 +1,12 @@
 RSpec.describe 'Restaurant', type: :request do
   let(:headers) { { HTTP_ACCEPT: 'application/json' } }
+  let(:Restaurant) { FactoryGirl.create(:restaurant) }
 
+  describe 'Get api/v1/restaurants' do
+    it 'returns collection of restaurants' do
+      get '/api/v1/restaurants'
 
+      expect(response.status).to eq 200
+    end
+  end
 end
