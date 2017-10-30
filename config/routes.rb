2bb097929276a2cc32aa9f56f8d903919b14b120
basil <basil.mawejje@gmail.com>
2017-10-30 16:59:15 +0200

    Add api controller. Add route to api. Add test for api.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-
+  namespace :api do
+    namespace :v0 do
+      resources :ping, only: [:index], constraints: { format: 'json' }
+    end
+  end
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
