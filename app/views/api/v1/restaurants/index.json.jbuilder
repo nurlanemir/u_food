b6915ec60596da37d7031b83eabbebaac49cc580
magnus-thor <magnus.thor.ar@gmail.com>
2017-10-31 16:46:39 +0100

    rspec test for json restaurant is green

--- app/views/api/v1/restaurants/index.json.jbuilder
+++ app/views/api/v1/restaurants/index.json.jbuilder
@@ -1,14 +1,14 @@
 json.data do
-  json.restaurant_categories @categories do |category|
-    json.restaurant_category_name category.name
-    json.restaurant_category_description category.description
-    json.restaurants @restaurants do |restaurant|
+  json.restaurants @restaurants do |restaurant|
+    json.restaurant_name restaurant.name
+    json.restaurant_address restaurant.address
+    json.restaurant_description restaurant.description
+    json.restaurant_latitude restaurant.latitude
+    json.restaurant_longitude restaurant.longitude
+    json.restaurant_categories @categories do |category|
       if restaurant.restaurant_category_id == category.id
-        json.restaurant_name restaurant.name
-        json.restaurant_address restaurant.address
-        json.restaurant_description restaurant.description
-        json.restaurant_latitude restaurant.latitude
-        json.restaurant_longitude restaurant.longitude
+        json.restaurant_category_name category.name
+        json.restaurant_category_description category.description
       end
     end
   end
--- spec/request/api/v1/restaurant_spec.rb
+++ spec/request/api/v1/restaurant_spec.rb
@@ -4,33 +4,24 @@ RSpec.describe 'Restaurant', type: :request do
   let(:headers) {{HTTP_ACCEPT: 'application/json'}}
 
   describe 'Get api/v1/restaurants' do
-    let(:restaurant_category) { FactoryGirl.create(:restaurant_category, name: 'Thai food', description: 'Thai food') }
-    let!(:restaurant) { FactoryGirl.create(:restaurant, name: 'My Thai', address: 'street 1', description: 'boring food', restaurant_category_id: restaurant_category.id) }
+    let(:restaurant_category) {FactoryGirl.create(:restaurant_category, name: 'Thai food', description: 'Thai food')}
+    let!(:restaurant) {FactoryGirl.create(:restaurant, name: 'My Thai', address: 'street 1', description: 'boring food', latitude: 33.7353997, longitude: 73.0781967, restaurant_category_id: restaurant_category.id)}
 
     it 'returns collection of restaurants' do
-      # restaurant.update(restaurant_category_id: restaurant_category.id)
 
       get '/api/v1/restaurants'
 
-      expected_response = {
-        'data' => {
-          'restaurant_categories'=> [
-            {
-              'restaurant_category_name'=> 'Thai food',
-              'restaurant_category_description'=> 'Thai food',
-              'restaurants'=> [
-                {
-                  'restaurant_name'=> 'My Thai',
-                  'restaurant_address'=> 'street 1',
-                  'restaurant_description'=> 'boring food',
-                  'restaurant_latitude'=>33.7353997,
-                  'restaurant_longitude'=>73.0781967
-                }
-              ]
-            }
-          ]
-        }
-      }
+      expected_response = {"data" =>
+                               {"restaurants" =>
+                                    [{"restaurant_name" => "My Thai",
+                                      "restaurant_address" => "street 1",
+                                      "restaurant_description" => "boring food",
+                                      "restaurant_latitude" => 33.7353997,
+                                      "restaurant_longitude" => 73.0781967,
+                                      "restaurant_categories" =>
+                                          [{"restaurant_category_name" => "Thai food",
+                                            "restaurant_category_description" => "Thai food"}]}]}}
+
 
       expect(response.status).to eq 200
       expect(response_json).to eq expected_response
