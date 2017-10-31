df0855e95f3bbbb5773f1c56ff120ca84c3e63a3
magnus-thor <magnus.thor.ar@gmail.com>
2017-10-31 15:35:29 +0100

    fixed merge conflicts

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'paperclip/matchers'
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -17,6 +18,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   config.include Warden::Test::Helpers
   config.include Paperclip::Shoulda::Matchers
+  config.include ResponseJSON
 end
 
 Shoulda::Matchers.configure do |config|
