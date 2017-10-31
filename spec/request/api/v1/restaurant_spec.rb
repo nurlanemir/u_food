24bc695fb8a948562f4c9ada5877aad0d0ce89d5
magnus-thor <magnus.thor.ar@gmail.com>
2017-10-31 12:03:43 +0100

    add rspec test for restaurant index json view

--- spec/request/api/v1/restaurant_spec.rb
+++ spec/request/api/v1/restaurant_spec.rb
@@ -1,12 +1,34 @@
 RSpec.describe 'Restaurant', type: :request do
-  let(:headers) { { HTTP_ACCEPT: 'application/json' } }
-  let(:Restaurant) { FactoryGirl.create(:restaurant) }
+  let(:headers) {{HTTP_ACCEPT: 'application/json'}}
 
   describe 'Get api/v1/restaurants' do
+    restaurant = FactoryGirl.create(:restaurant, name: 'My Thai', address: 'street 1', description: 'boring food')
+    restaurant_category = FactoryGirl.create(:restaurant_category, name: 'Thai food', description: 'Thai food')
+    restaurant.update(restaurant_category_id: restaurant_category)
+
     it 'returns collection of restaurants' do
       get '/api/v1/restaurants'
 
+      expected_response = {
+          'data': {
+        'restaurant_categories': [
+            {
+                'restaurant_category_name': 'Thai food',
+                'restaurant_category_description': 'Thai food',
+                'restaurants': [
+                    {
+                        'restaurant_name': 'My Thai',
+                        'restaurant_address': 'street 1',
+                        'restaurant_decription': 'boring food'
+                    }
+                ]
+            }
+          ]
+        }
+      }
+
       expect(response.status).to eq 200
+      expect(response_json).to eq expected_response
     end
   end
 end
